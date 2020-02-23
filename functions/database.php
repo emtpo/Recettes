3af3dfd2f94318610ca7e302e3cdd1c4077e29fa
emtpo <thepaut.emma@gmail.com>
2020-02-23 21:22:16 +0100

    first commit

---
+++ functions/database.php
@@ -0,0 +1,11 @@
+<?php
+$DB_HOST = "localhost";
+$DB_NAME = "tchat-esd";
+$DB_USER = "root";
+$DB_PASSWORD = "";
+try {
+    $db = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME", $DB_USER, $DB_PASSWORD);
+} catch (PDOException $e) {
+    print "Erreur !: " . $e->getMessage() . "<br/>";
+    die();
+}
